d98c4aad6066b02cc7f8bdeeb2630cd7e9af8e60
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2017-08-24 17:03:36 -0500

    Update the API to use a single object for...
    ...easier programmatic usage in the future

--- trytravis.py
+++ trytravis.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" Send your local git repo changes to Travis CI without needless commits and pushes. """
+""" Send your local git repo changes to Travis CIwithout needless commits and pushes. """
 
 import argparse
 import sys
@@ -29,31 +29,45 @@ __license__ = 'Apache-2.0'
 __url__ = 'https://github.com/SethMichaelLarson/trytravis'
 __version__ = '0.0.0.dev0'
 
-__all__ = ['main']
+__all__ = ['main', 'TryTravis']
 
 
-def load_personal_access_token(token)
-    raise NotImplementedError()
+class TryTravis(object):
+    """ Object which can be used to submit jobs via `trytravis` programmatically. """
+    def __init__(self, path, output=False):
+        self.path = path
+        self.output = output
+        self.token = None
+        self.remote = None
+        self.build = None
+        self.build_url = None
+        
+    def start(self, watch=False):
+        self._load_personal_access_token()
+        self._check_personal_access_token()
+        self._load_trytravis_github_repo()
+        self._submit_project_to_github()
+        self._wait_for_travis_build()
+        if watch:
+            self._watch_travis_build()
+        
+    def _load_personal_access_token(self)
+        raise NotImplementedError()
 
+    def _check_personal_access_token(self):
+        raise NotImplementedError()
 
-def check_personal_access_token(token):
-    raise NotImplementedError()
+    def _load_trytravis_github_repo(self):
+        raise NotImplementedError()
 
+    def _submit_project_to_github(self):
+        raise NotImplementedError()
 
-def check_trytravis_github_repo(token):
-    raise NotImplementedError()
+    def _wait_for_travis_build(self):
+        raise NotImplementedError()
 
-
-def submit_project_to_github(token, path):
-    raise NotImplementedError()
-
-
-def wait_for_travis_build(commit):
-    raise NotImplementedError()
-
-
-def watch_travis_build(build):
-    raise NotImplementedError()
+    def watch_travis_build(self):
+        raise NotImplementedError()
 
 
 def main(argv=None):
@@ -61,4 +75,6 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
+    trytravis = TryTravis(os.getcwd(), output=True)
+    trytravis.start(watch=True)
     sys.exit(0)
